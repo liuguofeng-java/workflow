c4a24edce199580614767297335318b3827d83b2
liuguofeng <liuguofeng-java@qq.com>
2023-10-27 17:31:33 +0800

    更新SQL文件

--- activiti7-java/sql/index.sql
+++ activiti7-java/sql/index.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80011
  File Encoding         : 65001
 
- Date: 27/10/2023 09:00:58
+ Date: 27/10/2023 17:31:03
 */
 
 SET NAMES utf8mb4;
@@ -40,6 +40,24 @@ CREATE TABLE `sys_user`  (
 -- ----------------------------
 INSERT INTO `sys_user` VALUES ('1', '0f45e0320d93832c90dd3ed323129dd6', '管理员', '123', 'admin', '1396198931@qq.com', '18888888888', '2023-10-16 13:49:00', '2023-10-26 14:26:47');
 INSERT INTO `sys_user` VALUES ('2', 'db3760209b816f6d350278dd2f3b1351', '测试账号', '123', 'test', '1396198931@qq.com', '18888888888', '2023-10-16 13:49:00', '2023-10-22 12:39:50');
+
+-- ----------------------------
+-- Table structure for sys_form
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_form`;
+CREATE TABLE `sys_form`  (
+  `form_id` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '表单id',
+  `form_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '表单名称',
+  `form_data` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '表单数据',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`form_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = 'activiti相关->流程表单' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_form
+-- ----------------------------
+INSERT INTO `sys_form` VALUES ('edf38c74a1705027fae46c842b1686b9', '测试1', '{\"widgetList\":[{\"key\":39587,\"type\":\"textarea\",\"icon\":\"textarea-field\",\"formItemFlag\":true,\"options\":{\"name\":\"textarea37414\",\"label\":\"textarea\",\"labelAlign\":\"\",\"rows\":3,\"defaultValue\":\"\",\"placeholder\":\"\",\"columnWidth\":\"200px\",\"size\":\"\",\"labelWidth\":null,\"labelHidden\":false,\"readonly\":false,\"disabled\":false,\"hidden\":false,\"required\":false,\"requiredHint\":\"\",\"validation\":\"\",\"validationHint\":\"\",\"customClass\":[],\"labelIconClass\":null,\"labelIconPosition\":\"rear\",\"labelTooltip\":null,\"minLength\":null,\"maxLength\":null,\"showWordLimit\":false,\"onCreated\":\"\",\"onMounted\":\"\",\"onInput\":\"\",\"onChange\":\"\",\"onFocus\":\"\",\"onBlur\":\"\",\"onValidate\":\"\"},\"id\":\"textarea37414\"},{\"key\":39030,\"type\":\"radio\",\"icon\":\"radio-field\",\"formItemFlag\":true,\"options\":{\"name\":\"radio88113\",\"label\":\"radio\",\"labelAlign\":\"\",\"defaultValue\":null,\"columnWidth\":\"200px\",\"size\":\"\",\"displayStyle\":\"inline\",\"buttonStyle\":false,\"border\":false,\"labelWidth\":null,\"labelHidden\":false,\"disabled\":false,\"hidden\":false,\"optionItems\":[{\"label\":\"radio 1\",\"value\":1},{\"label\":\"radio 2\",\"value\":2},{\"label\":\"radio 3\",\"value\":3}],\"required\":true,\"requiredHint\":\"请输入\",\"validation\":\"\",\"validationHint\":\"\",\"customClass\":[],\"labelIconClass\":null,\"labelIconPosition\":\"rear\",\"labelTooltip\":null,\"onCreated\":\"\",\"onMounted\":\"\",\"onChange\":\"\",\"onValidate\":\"\"},\"id\":\"radio88113\"}],\"formConfig\":{\"modelName\":\"formData\",\"refName\":\"vForm\",\"rulesName\":\"rules\",\"labelWidth\":80,\"labelPosition\":\"top\",\"size\":\"\",\"labelAlign\":\"label-left-align\",\"cssCode\":\"\",\"customClass\":[],\"functions\":\"\",\"layoutType\":\"PC\",\"jsonVersion\":3,\"onFormCreated\":\"\",\"onFormMounted\":\"\",\"onFormDataChange\":\"\"}}', '2023-10-27 16:58:00', '2023-10-27 17:28:47');
 
 -- ----------------------------
 -- Table structure for sys_dept
